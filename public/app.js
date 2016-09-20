d71e04f6f2e08504e0871d7492450795c6911e37
Ashley Winn <ashley.winn@gmail.com>
2016-09-20 22:36:07 +0800

    Added application toolbar

--- public/app.js
+++ public/app.js
@@ -16,6 +16,10 @@ learnjs.template = function(name) {
   return $('.templates .' + name).clone();
 }
 
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
+}
+
 learnjs.applyObject = function(obj, elem) {
   for (var key in obj) {
     elem.find('[data-name="' + key + '"]').text(obj[key]);
@@ -27,6 +31,10 @@ learnjs.flashElement = function(elem, content) {
     elem.html(content);
     elem.fadeIn();
   });
+}
+
+learnjs.landingView = function() {
+  return learnjs.template('landing-view');
 }
 
 learnjs.buildCorrectFlash = function(problemNum) {
@@ -65,16 +73,29 @@ learnjs.problemView = function(data) {
   view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
   learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
+  
+  if (problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
+  
   return view;
 }
 
 learnjs.showView = function(hash) {
   var routes = {
-    '#problem': learnjs.problemView
+    '#problem': learnjs.problemView,
+    '#'       : learnjs.landingView,
+    ''        : learnjs.landingView
   };
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
   if (viewFn) {
+    learnjs.triggerEvent('removingView', []);
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
